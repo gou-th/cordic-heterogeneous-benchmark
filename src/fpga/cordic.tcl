62a01b23248e7156511198e27752e1caccec9c37
Goutham Ganesh Kumar <87598822+gou-th@users.noreply.github.com>
2026-04-24 02:59:45 +0530

    Update cordic.tcl with new xdc file name

--- src/fpga/cordic.tcl
+++ src/fpga/cordic.tcl
@@ -190,9 +190,9 @@ if {[string equal [get_filesets -quiet constrs_1] ""]} {
 set obj [get_filesets constrs_1]
 
 # Add/Import constrs file and set constrs file properties
-set file "[file normalize "$origin_dir/cordic.xdc"]"
+set file "[file normalize "$origin_dir/cordic_xdc.xdc"]"
 set file_imported [import_files -fileset constrs_1 [list $file]]
-set file "cordic.xdc"
+set file "cordic_xdc.xdc"
 set file_obj [get_files -of_objects [get_filesets constrs_1] [list "*$file"]]
 set_property -name "file_type" -value "XDC" -objects $file_obj
 
